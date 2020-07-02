db9f392557da86fc5e6f3d4de83fe0fbd676adb2
galileoguzman <galileoguzman@gmail.com>
2020-07-01 21:22:28 -0500

    Change print statement for csv row

--- csv_workers_reader.py
+++ csv_workers_reader.py
@@ -4,4 +4,4 @@ file_name = 'workers.csv'
 with open(file_name) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        print(row)
+        print(f'{type(row)} - {row}')
